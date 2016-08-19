ffcb5ab8a7408cc6cf88ed1759e3a92087d4a514
Aleksey Lovchikov <svdmusic@gmail.com>
2016-08-19 17:44:36 +0300

    add responsive for form

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 /**
- * Created by Andrew on 19.08.2016.
+ * Created by Aleksey Lovchikov on 19.08.2016.
  */
 
 $(document).ready(function() {
+    // init variables
     var searchInput = $("input[type='text']"),
         remove = $('#remove'),
         submitBtn = $('button[type="submit"]');
 
+    // clear input, search result
     function clear() {
         remove.css({display: 'none'});
         submitBtn.attr('disabled', 'disabled');
@@ -15,6 +17,7 @@ $(document).ready(function() {
             .html('');
     }
 
+    // listen keypress in input
     searchInput.keyup(function() {
         var txt = $(this).val();
         if (txt !== '') {
@@ -23,7 +26,6 @@ $(document).ready(function() {
 
             if (/(((http|ftp|https):\/{2})+(([0-9a-z_-]+\.)+(aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cx|cy|cz|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mn|mn|mo|mp|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|nom|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ra|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw|arpa)(:[0-9]+)?((\/([~0-9a-zA-Z\#\+\%@\.\/_-]+))?(\?[0-9a-zA-Z\+\%@\/&\[\];=_-]+)?)?))\b/.test(txt)) {
                 var onlyDomain = txt.replace(/^(http|https):\/\//, '');
-                // onlyDomain = onlyDomain.replace(/(https?:\/\/)?(www.)?([A-Za-z]+\.){1,}([A-Za-z]{2,})/?//, '');
 
                 $('#result')
                     .css({display: 'block'})
@@ -52,6 +54,7 @@ $(document).ready(function() {
         clear();
     });
 
+    // ajax query
     $('form').submit(function(e) {
         e.preventDefault();
         var searchVal = encodeURI(searchInput.val());
@@ -77,16 +80,20 @@ $(document).ready(function() {
     });
 });
 
-var inArray = Array.prototype.inArray = function(el) {
-    var result = false;
-    (arguments[1] || this).forEach(function(val) {
-        if (el === val) {
-            result = true;
-        }
-    });
 
-    return result;
-};
+
+
+
+// var inArray = Array.prototype.inArray = function(el) {
+//     var result = false;
+//     (arguments[1] || this).forEach(function(val) {
+//         if (el === val) {
+//             result = true;
+//         }
+//     });
+//
+//     return result;
+// };
 
 // console.log([10, 12, 50, 99, 9, 99].inArray(105)); // false
 // console.log(inArray(1, [10, 1, 105])); // true
